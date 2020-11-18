a7c92c183c47b7a2886c356af375e3adf64afac5
Bryan Urias <bryanurias@Bryans-Air.attlocal.net>
2020-11-18 12:50:10 -0600

    added changes to allow Erins card to populate data

--- frontend/src/Components/JobAppCards.js
+++ frontend/src/Components/JobAppCards.js
@@ -1,31 +1,66 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useAuth } from "../Context/AuthContext"
+import axios from "axios"
+import { auth } from "../firebase"
 
 export default function JobCards() {
 
     const { currentUser } = useAuth()
+    const [jobs, setJobs] = useState([])
+
+    useEffect(() => {
+      const fetchData = async() => {
+       
+        const idToken = await auth.currentUser.getIdToken(/* forceRefresh */ true) 
+          
+
+            const results = await axios('http://localhost:8000/job-apps',
+            {
+              
+               
+                headers: { 
+                "Authorization": `Bearer ${idToken}` }
+            })
+            console.log(results.data[0])
+            setJobs(results.data)
+         
+      
+
+      }
+fetchData()
+    }, [])
 
 
     return ["Light"].map((variant, idx) => (
-      <Card
-        bg={variant.toLowerCase()}
-        key={idx}
-        text={variant.toLowerCase() === "light" ? "dark" : "white"}
-        style={{ width: "18rem" }}
-        className="mb-2"
-      >
-    <Card.Header>user {currentUser.uid} </Card.Header>
-
-        <Card.Body>
-          <Card.Title>hi there </Card.Title>
-          <Card.Text>
-            yeet 
-            <br></br>
-            meep
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <div>
+      { jobs.map((job) => {
+        return (
+          <Card
+          bg={variant.toLowerCase()}
+          key={idx}
+          text={variant.toLowerCase() === "light" ? "dark" : "white"}
+          style={{ width: "18rem" }}
+          className="mb-2"
+        >
+      
+  
+          <Card.Body>
+          <Card.Header>user {job.user_id} </Card.Header>
+            <Card.Title>{job.job_title}</Card.Title>
+            <Card.Text>
+              {job.company_name}
+              <br></br>
+              {job.date_applied}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        )
+      })
+      
+    }
+    </div>
     ));
+      
   }
