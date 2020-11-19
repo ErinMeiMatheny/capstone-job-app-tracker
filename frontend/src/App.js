11dc638529f6a5cae5f94b5ef66d5d5969ad5eb8
Bryan Urias <bryanurias@Bryans-Air.attlocal.net>
2020-11-19 09:08:42 -0600

    working on UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from "./Components/Home/Home"
 import HiddenRoute from "./Components/HiddenRoute"
 import LandingPage from "./Components/Landing Page/LandingPage"
 import Nav from "./Components/Nav/Nav"
+import Example from "./Components/SignInModal/SignInModal"
 
 //State
 import { AuthProvider } from "./Context/AuthContext";
@@ -16,6 +17,7 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 //Bootstrap imports
 import { Container } from "react-bootstrap";
+import SignInSide from "./Components/SignInV2/Signin";
 
 function App() {
   return (
@@ -28,7 +30,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={LandingPage} />
               <Route path="/signup" component={Signup} />
-              <Route path="/signin" component={Signin} />
+              <Route path="/signin" component={SignInSide} />
               <HiddenRoute path="/home" component={Home} />
 
             </Switch>
